945971d3f3dae7788717a33475838f5302c4725d
cipherswan <leskauskas25@gmail.com>
2017-12-10 16:56:31 +0200

    Add jUnit tests for Sphere class

--- src/main/java/Sphere.java
+++ src/main/java/Sphere.java
@@ -1,3 +1,11 @@
-public class Sphere {
+class Sphere {
+    private double r;
 
+    Sphere(double r) {
+        this.r = r;
+    }
+
+    private double sferosPlotas(){
+        return r;
+    }
 }
--- src/test/java/SphereTest.java
+++ src/test/java/SphereTest.java
@@ -1,5 +1,47 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 public class SphereTest {
+
+    @Test (expected = IllegalArgumentException.class)
+    public void kaiSpindulysNulisSkaiciuojantPlotaIsmetamasException(){
+
+    }
+
+    @Test
+    public void kaiSpidulys2YraGaunamasSferosPlotas(){
+        Sphere sp = new Sphere(2);
+    }
+
+    @Test
+    public void kaiSpidulys4YraGaunamasSferosPlotas(){
+        Sphere sp = new Sphere(4);
+    }
+
+    @Test
+    public void kaiSpidulys6YraGaunamasSferosPlotas(){
+        Sphere sp = new Sphere(6);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void kaiSpindulysNulisSkaiciuojantTuriIsmetamasException(){
+
+    }
+
+
+    @Test
+    public void kaiSpidulys2YraGaunamasSferosTuris(){
+        Sphere sp = new Sphere(2);
+    }
+
+    @Test
+    public void kaiSpidulys4YraGaunamasSferosTuris(){
+        Sphere sp = new Sphere(4);
+    }
+
+    @Test
+    public void kaiSpidulys6YraGaunamasSferosTuris(){
+        Sphere sp = new Sphere(6);
+    }
 
 }
