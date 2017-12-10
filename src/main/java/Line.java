3c1f22653058275d0d635d33192e593d6b562afa
cipherswan <leskauskas25@gmail.com>
2017-12-10 14:52:21 +0200

    Add Line class

--- src/main/java/Line.java
+++ src/main/java/Line.java
@@ -1,6 +1,26 @@
 class Line {
 
-    double findSlope(double x1, double y1, double x2, double y2){
+    private double x1, y1, x2, y2;
+
+    public Line(double x1, double y1, double x2, double y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+
+    double rastiNuolydi() {
         return (y2-y1)/(x2-x1);
+    }
+
+    double rastiIlgi() {
+        return Math.sqrt( (x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2) );
+    }
+
+    boolean arLygiagrecios(Line line) {
+        if (line.rastiNuolydi()==this.rastiNuolydi())
+            return true;
+        else
+            return false;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,39 +6,27 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         int choice;
-        Line line = new Line();
+        Line line = new Line(2, 1, 4, 7);
+        Line line2 = new Line(2, 1, 4, 7);
 
         do{
-            System.out.print("1. Tiesės krypties koeficientas \n");
-            System.out.print("2. Sfera \n");
-            System.out.print("3. Išeiti \n");
+            System.out.print("1. Tiesės krypties koeficientas / ilgis \n");
+            System.out.println("2. Ar tiesės lygiagrečios");
+            System.out.print("3. Sfera \n");
+            System.out.print("4. Išeiti \n");
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             choice = Integer.parseInt(br.readLine());
             switch (choice) {
                 case 1:{
-                    System.out.println("Įveskite koordinates: ");
-
-                    System.out.print("x1: ");
-                    BufferedReader brx1 = new BufferedReader(new InputStreamReader(System.in));
-                    double x1 = brx1.read();
-
-                    System.out.print("y1: ");
-                    BufferedReader bry1 = new BufferedReader(new InputStreamReader(System.in));
-                    double y1 = bry1.read();
-
-                    System.out.print("x2: ");
-                    BufferedReader brx2 = new BufferedReader(new InputStreamReader(System.in));
-                    double x2 = brx2.read();
-
-                    System.out.print("y2: ");
-                    BufferedReader bry2 = new BufferedReader(new InputStreamReader(System.in));
-                    double y2 = bry2.read();
-
-                    System.out.println("Krypties koeficientas: " + line.findSlope(x1, y1, x2, y2));
+                    System.out.println("Tiesės krypties koeficientas: " + line.rastiNuolydi());
+                    System.out.println("Tiesės ilgis: " + line.rastiIlgi());
                 }
                 break;
                 case 2:{
-
+                    System.out.println(line.arLygiagrecios(line2));
+                }
+                break;
+                case 3:{
 
                 }
                 break;
@@ -46,7 +34,7 @@ public class Main {
                     break;
                 }
             }
-        }while(choice!=3);
+        }while(choice!=4);
 
     }
 }
